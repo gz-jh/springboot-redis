540bb4a0294f7273fbaee4e3f0b33206fb89555e
gzjh <gz950516@163.com>
2019-06-29 15:14:15 +0800

    支付模块开发

---
+++ src/main/java/com/offcn/pay/paylogon.java
@@ -0,0 +1,13 @@
+package com.offcn.pay;
+
+import java.util.Map;
+
+public class paylogon {
+    public void payweixin(String out){
+        System.out.println("微信制服");
+    }
+    public Map aueryStatus(){
+        System.out.println("查询支付状态");
+        return null;
+    }
+}
